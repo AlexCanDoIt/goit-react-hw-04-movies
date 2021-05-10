69ac43a2a531e843b9c9ee2f2fb2745fb23fdf01
AlexCanDoIt <alexcandoitt@gmsil.com>
2021-05-10 11:44:13 +0300

    Update code

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,5 +1,7 @@
+import styles from './Loader.module.css'
+
 const Loader = () => (
-  <h1 className="title">It's gonna be legen... wait for it ...dary. Legendary!</h1>
+  <h1 className={styles.title}>It's gonna be legen... wait for it ...dary. Legendary!</h1>
 );
 
 export default Loader;
